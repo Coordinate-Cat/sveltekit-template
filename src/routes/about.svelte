37b6f924894f0e30329cc57dc10121f08e22ef1a
Coordinate-Cat <tetrapasta02@gmail.com>
2021-04-29 17:24:44 +0900

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="content">
+	<h1>About this app</h1>
+</div>
+
+<style>
+</style>
